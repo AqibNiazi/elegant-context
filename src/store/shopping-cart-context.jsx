2ce719dd0d12b8b8947a31666e9f5921b2ff0714
AqibNiazi <aqibjaved5201@gmail.com>
2025-03-30 17:11:25 +0500

    React Context added to all components to remove props drilling

--- src/store/shopping-cart-context.jsx
+++ src/store/shopping-cart-context.jsx
@@ -3,5 +3,5 @@ import { createContext } from "react";
 export const CartContext = createContext({
   items: [], //This is the default value for the context that helps to get better auto completion.
   addItemsToCart: (id) => {}, //This is the default value for the context that helps to get better auto completion.
-  removeItemsFromCart: (id) => {}, //This is the default value for the context that helps to get better auto completion.
+  updateItemQuantity: (id) => {}, //This is the default value for the context that helps to get better auto completion.
 }); //Create context return an oject which further contains a component.
